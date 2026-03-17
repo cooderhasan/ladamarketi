76a88a570d27e29279c4420ce264e8ef8cdcd533
Hasan Cooder <cooderhasan@gmail.com>
2026-03-17 14:27:21 +0300

    feat: add 301 redirect from non-www to www for SEO

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,21 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'ladamarketi.com',
+          },
+        ],
+        destination: 'https://www.ladamarketi.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
